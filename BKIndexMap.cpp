a4ed26e81c9ebaf49421ae0a8c9766befb995b35
Mike <Mike@fake.com>
2021-09-17 12:34:47 +0100

    Initial attempt.
    Limited map member functions exposed.
    Only one layer of indexing.

---
+++ BKIndexMap.cpp
@@ -0,0 +1,204 @@
+#include "BkIndexMap.h"
+#include <benchmark/benchmark.h>
+#include <gtest/gtest.h>
+
+void BkIdxMapBase::Lock()
+{
+    // indicate you want to mutate and wait for the reply from the background thread
+    if (m_state == State::Mutating)
+    {
+        m_state = State::MutateBegin;
+        while(m_state!=State::Stable)
+            /*Wait*/;
+        m_state = State::Mutating;
+    }
+}
+
+void BkIdxMapBase::Unlock()
+{
+    m_state = State::Mutated;
+}
+
+
+
+TEST(IndexedMap, Construct)
+{
+    BkIdxMap<int,int> simple_map;
+}
+
+TEST(IndexedMap, Initailise)
+{
+    BkIdxMap<int,int> simple_map;
+    for (int i = 0; i < 1000; ++i)
+        simple_map.Add(i, i+1);
+}
+
+TEST(IndexedMap, InitailiseAndWait)
+{
+    BkIdxMap<int,int> simple_map;
+    for (int i = 0; i < 1000; ++i)
+        simple_map.Add(i, i+1);
+    while (!simple_map.IsFastIndexAvailable())
+       /*wait*/;
+}
+
+
+TEST(IndexedMap, Reindex)
+{
+    BkIdxMap<int,int> simple_map;
+    for (int i = 0; i < 1000; ++i)
+        simple_map.Add(i, i+1);
+    EXPECT_FALSE(simple_map.IsFastIndexAvailable());
+    // add more
+    for (int i = 1000; i < 1010; ++i)
+        simple_map.Add(i, i+1);
+    //but still finish
+    EXPECT_FALSE(simple_map.IsFastIndexAvailable());
+    while (!simple_map.IsFastIndexAvailable())
+       /*wait*/;
+}
+
+TEST(IndexedMap, LockFill)
+{
+    BkIdxMap<int,int> simple_map;
+    simple_map.PerformLocked( [&simple_map](){
+    for (int i = 0; i < 1000*1000; ++i)
+        simple_map.Add(i, i+1);
+    });
+}
+
+TEST(IndexedMap, Indexing)
+{
+    BkIdxMap<int,int> simple_map;
+    for (int i = 0; i < 1000; ++i)
+        simple_map.Add(i, i+1);
+    EXPECT_FALSE(simple_map.IsFastIndexAvailable());
+    for (int i = 0; i < 1000; ++i)
+    {
+        const auto found = simple_map.Find(i);
+        EXPECT_EQ(found->first, i);
+    }
+    while (!simple_map.IsFastIndexAvailable())
+       /*wait*/;
+    for (int i = 0; i < 1000; ++i)
+    {
+        const auto found = simple_map.Find(i);
+        EXPECT_EQ(found->first, i);
+    }
+    EXPECT_TRUE(simple_map.IsFastIndexAvailable());
+
+    for (int i = 1000; i < 2000; ++i)
+        simple_map.Add(i, i+1);
+    for (int i = 0; i < 2000; ++i)
+    {
+        const auto found = simple_map.Find(i);
+        EXPECT_EQ(found->first, i);
+    }
+}
+
+//----------------- Timing Functions -------------------
+
+static void FillLocked(benchmark::State &state)
+{
+    for (auto _ : state)
+    {
+        static BkIdxMap<int,int> map;
+        map.PerformLocked( [&](){
+            for (int i = 0; i < state.range(0); ++i)
+                map.Add(i, i+1);
+            });
+    }
+}
+
+static void FillUnlocked(benchmark::State &state)
+{
+    for (auto _ : state)
+    {
+        static BkIdxMap<int,int> map;
+        for (int i = 0; i < state.range(0); ++i)
+            map.Add(i, i+1);
+    }
+}
+
+static void IndexingTime(benchmark::State &state)
+{
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+        static BkIdxMap<int,int> map;
+        map.PerformLocked( [&](){
+            for (int i = 0; i < state.range(0); ++i)
+                map.Add(i, i+1);
+            });
+        state.ResumeTiming();
+        while (!map.IsFastIndexAvailable())
+            /*Wait*/;
+    }
+}
+
+static void LookupNoIndex(benchmark::State &state)
+{
+    static BkIdxMap<int,int> map;
+    if (map.Size()!=(size_t)state.range(0))
+    {
+        map.Clear();
+        map.PerformLocked( [&](){
+            for (int i = 0; i < state.range(0); ++i)
+                map.Add(i, i+1);
+            map.DisableIndexing();
+            });
+    }
+    for (auto _ : state)
+    {
+        int i = rand()%state.range(0);
+        benchmark::DoNotOptimize(map.Find(i));
+    }
+}
+
+
+static void LookupIndexBuilt(benchmark::State &state)
+{
+    static BkIdxMap<int,int> map;
+    if (map.Size()!=(size_t)state.range(0))
+    {
+        map.Clear();
+        map.PerformLocked( [&](){
+            for (int i = 0; i < state.range(0); ++i)
+                map.Add(i, i+1);
+            });
+    }
+    while (!map.IsFastIndexAvailable())
+        ;
+    for (auto _ : state)
+    {
+        int i = rand()%state.range(0);
+        benchmark::DoNotOptimize(map.Find(i));
+    }
+}
+
+static void LookupIndexMixed(benchmark::State &state)
+{
+    static BkIdxMap<int,int> map;
+    if (map.Size()!=(size_t)state.range(0))
+    {
+        map.Clear();
+        map.PerformLocked( [&](){
+            for (int i = 0; i < state.range(0); ++i)
+                map.Add(i, i+1);
+            });
+    }
+    for (auto _ : state)
+    {
+        int i = rand()%state.range(0);
+        benchmark::DoNotOptimize(map.Find(i));
+    }
+}
+
+constexpr int MAX = 16*1000*1000;
+constexpr int FILLMAX = 1*1000*1000;
+BENCHMARK(FillLocked)->Range(FILLMAX, FILLMAX);
+BENCHMARK(FillUnlocked)->Range(FILLMAX, FILLMAX);
+BENCHMARK(IndexingTime)->Range(FILLMAX, FILLMAX);
+BENCHMARK(LookupIndexBuilt)->Range(MAX, MAX);
+BENCHMARK(LookupIndexMixed)->Range(MAX, MAX);
+BENCHMARK(LookupNoIndex)->Range(MAX, MAX);
---
+++ BkIndexMap.h
@@ -0,0 +1,242 @@
+#include <map>
+#include <atomic>
+#include <vector>
+#include <thread>
+#include <condition_variable>
+#include <iostream>
+
+/********************************************************************************************************
+ * Not production code
+ * 
+ * BkIdxMap : A wrapper class for a map. The idea is to combine the flexibility of a map, with the speed of
+ * a vector. This is acheived by using a another thread to build a vector-index into the map, using background processing
+ * This in many setups is "free", as few existing applications use multi-threading effectively. It will however use more
+ * memory, and the syncronisation (via an atomic), is not free.
+ * If the index is not built yet, the native map find is used.
+ * 
+ * threading
+ * ---------
+ * In a real-world system, allocating a thread per map-instance is impractical, and the thread loop should be converted
+ * to a "task", to be added to a task-pool (std::async for example could be used to do this).
+ * A quick  fix, might be to make the thread static (one thread for all instances)
+ * 
+ * states
+ * ------
+ * The model used is a "mater/slave" relationship, where the background thread is only capable of changing
+ * mutated->indexing
+ * indexing->stable
+ * Using a compare/exchange to enforce this, means that if the master thread changes the state when the background thread
+ * is busy, we never over-write the change because of a background thread event.
+ * This should mean the background thread will eventually do the right thing. (and eventual consistancy is all we promise)
+ * 
+ * memory
+ * ======
+ * We use the default model of expanding vector storage only. This means that the system quickly hits a state where no 
+ * allocations are happening, but it does mean that we almost always allocate more memory than required.
+ * It would be trivial to change the system to allow "shrink to fit" operations on the vector
+ * 
+ * Locking
+ * =======
+ * You may need to perform a block of operations on the structure, and wish to avoid the overhead of repeatedly locking/unlocking.
+ * Locking is cheap (it's an atomic) but cheap is not free. 
+ * A PerformLocked is structured way to pass a arbitrary function to be performed whilst locked. While this looks messy, it has
+ * the advantage of being impossible to break.
+ * Recursive locks/unlocks are allowed.(so you can for example call member functions from PerformLocked)
+ * Whilst locked the background thread will idle.
+ * 
+ * 
+ * ToDo
+ * ----
+ * Add a second layer of indexing, time to see the speed impact.
+ * Add more map functions (range-insert would be a good one for example)
+********************************************************************************************************/
+
+
+class BkIdxMapBase
+{
+protected:
+    enum class State
+    {
+        Stable,   // indexing is complete (or aborted)
+        MutateBegin, // we are waiting for the bk thread to abort
+        Mutating, // we are waiting for the bk thread to abort
+        Mutated,  // index requires rebuilding
+        Indexing,  // indexis being built
+        Quit,
+        QuitDone
+    };
+    std::atomic<State> m_state{State::Stable};
+
+    // abstract operations, in case more complex (say a mutex) based solution is required later.
+    // these are "publuc facing" functions for the main thread. Abstracted via LockRAII
+    void Lock();
+    void Unlock();
+
+    // this is the background thread equivelent of the lock.
+    struct LockPrivate
+    {
+        BkIdxMapBase &base;
+        std::unique_lock<std::mutex> lg;
+        LockPrivate(BkIdxMapBase &b)
+            : base(b)
+        {
+        }
+        ~LockPrivate()
+        {
+        }
+        void Wait() // wait for mutation
+        {
+            while (base.m_state==State::Stable)
+                /*Sleep?*/;
+        }
+        void Release()
+        {
+        }
+    };
+
+public:
+    struct LockRAII
+    {
+        BkIdxMapBase &base;
+        LockRAII(BkIdxMapBase &b)
+            : base(b)
+        {
+            base.Lock();
+        }
+        ~LockRAII()
+        {
+            base.Unlock();
+        }
+    };
+    bool IsFastIndexAvailable() const { return m_state == State::Stable; }
+};
+
+template <class K, class V>
+class BkIdxMap : BkIdxMapBase
+{
+    using Impl = std::map<K, V>;
+    using Iterator = typename Impl::iterator;
+    struct Elem : public std::pair<K, Iterator>
+    {
+        Elem( const K& k, const Iterator& i): std::pair<K,Iterator>(k,i) {}
+        bool operator<(const K& k) const { return std::pair<K, Iterator>::first < k; }
+    };
+    Impl m_impl;
+    std::vector<Elem> m_index;
+    std::thread m_idxThread;
+
+public:
+    BkIdxMap();
+    ~BkIdxMap();
+    //PerformLocked assumes the structure is mutated
+    template <class Fn>
+    void PerformLocked(Fn &&fn) {BkIdxMapBase::LockRAII lock(*this); fn();}
+
+    inline void Add(const K &, const V &);
+    inline void Remove(const K &);
+    inline Iterator Find(const K &);
+    inline bool IsFastIndexAvailable() const { return m_state == State::Stable; }
+
+    Iterator End() { return m_impl.end(); }
+    Iterator Begin() { return m_impl.begin(); }
+    Iterator End() const { return m_impl.end(); }
+    Iterator Begin() const { return m_impl.begin(); }
+
+    bool Empty() const noexcept { return m_impl.empty(); }
+    std::size_t Size() const noexcept { return m_impl.size(); }
+    void Clear() noexcept { BkIdxMapBase::LockRAII lock(*this); m_impl.clear(); }
+
+    // for timing only, cannot be undone
+    void DisableIndexing() { m_state = State::Quit; m_idxThread.join(); }
+};
+
+template <class K, class V>
+BkIdxMap<K,V>::BkIdxMap()
+{
+    m_idxThread = std::thread([this](){
+        Iterator progress;
+        while(true)
+        {
+            BkIdxMapBase::LockPrivate priv_lock(*this);
+            State expected;
+            switch(m_state)
+            {
+                // respond to signals, the main thread is in a busy wait state, so no exchange required
+                case State::Quit:
+                    m_state = State::QuitDone;
+                    return;
+                case State::MutateBegin:
+                    progress = m_impl.end();
+                    m_state = State::Stable;
+                    break;
+                case State::QuitDone:
+                    /*NEVER HANDLED*/;
+                    break;
+                // states that mean we are waiting on the other thread to do something
+                case State::Mutating:
+                    priv_lock.Wait();
+                    break;
+                case State::Stable:
+                    priv_lock.Wait();
+                    break;
+                // processing states. If other thread forces a state change, respect thier change (compare/exchange)
+                case State::Mutated:
+                    m_index.clear();
+                    progress = m_impl.begin();
+                    expected = State::Mutated;
+                    m_state.compare_exchange_strong(expected, State::Indexing);
+                    break;
+                case State::Indexing:
+                    m_index.emplace_back(progress->first, progress);
+                    progress++;
+                    if (progress==m_impl.end())
+                    {
+                        expected = State::Indexing;
+                        m_state.compare_exchange_strong(expected, State::Stable);
+                    }
+                    break;
+            }
+        }
+    });
+}
+
+template <class K, class V>
+inline BkIdxMap<K,V>::~BkIdxMap()
+{
+    if (m_idxThread.joinable())
+    {
+        m_state = State::Quit;
+        while (m_state!=State::QuitDone)
+            /*Wait*/;
+        m_idxThread.join();
+    }
+}
+
+template <class K, class V>
+inline void BkIdxMap<K,V>::Add(const K& k, const V& v)
+{
+    LockRAII lock(*this);
+    m_impl.insert( std::make_pair( k, v) );
+}
+
+template <class K, class V>
+inline void BkIdxMap<K,V>::Remove(const K& k)
+{
+    LockRAII lock(*this);
+    m_impl.erase( k );
+}
+
+template <class K, class V>
+inline typename BkIdxMap<K,V>::Iterator BkIdxMap<K,V>::Find(const K& k)
+{
+    if (IsFastIndexAvailable())
+    {
+        const auto found  = std::lower_bound(m_index.begin(), m_index.end(), k);
+        if (found == m_index.end() || (found->first!=k))
+            return m_impl.end();
+        return found->second;
+    }
+    else
+        return m_impl.find(k);
+}
+ 
